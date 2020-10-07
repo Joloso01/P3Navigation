ae37656ce7f2d432f77f2d24c38a371bfca02a71
jose <jbermejop01@elpuig.xeill.net>
2020-10-07 20:28:05 +0200

    TabLayout

--- app/src/main/java/com/example/p3navigation/BottomActivity.java
+++ app/src/main/java/com/example/p3navigation/BottomActivity.java
@@ -1,14 +1,26 @@
 package com.example.p3navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.p3navigation.databinding.ActivityBottomBinding;
+
 public class BottomActivity extends AppCompatActivity {
+    ActivityBottomBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom);
+        setContentView((binding=ActivityBottomBinding.inflate(getLayoutInflater())).getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController =((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController);
     }
 }
