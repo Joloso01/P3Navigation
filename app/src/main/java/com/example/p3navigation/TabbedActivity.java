6bd42a86c43767a326b30a2ada16d64f13b8a891
jose <jbermejop01@elpuig.xeill.net>
2020-10-07 19:21:23 +0200

    Navigation View

---
+++ app/src/main/java/com/example/p3navigation/TabbedActivity.java
@@ -0,0 +1,14 @@
+package com.example.p3navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TabbedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabbed);
+    }
+}
