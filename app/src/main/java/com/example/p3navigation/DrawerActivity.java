9047fadb0ac0243a2e7b89b728721bc97272c3d3
jose <jbermejop01@elpuig.xeill.net>
2020-10-07 19:58:00 +0200

    Bottom Navigation View

--- app/src/main/java/com/example/p3navigation/DrawerActivity.java
+++ app/src/main/java/com/example/p3navigation/DrawerActivity.java
@@ -1,14 +1,33 @@
 package com.example.p3navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.example.p3navigation.databinding.ActivityDrawerBinding;
+import com.example.p3navigation.databinding.ActivityMainBinding;
+
 public class DrawerActivity extends AppCompatActivity {
+    ActivityDrawerBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drawer);
+        setContentView((binding = ActivityDrawerBinding.inflate(getLayoutInflater())).getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.drawer1Fragment, R.id.drawer2Fragment
+        )
+                .setOpenableLayout(binding.drawerLayout)
+                .build();
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration);
     }
 }
